7d16093219ff6834cb1e1f491b27d0886f5dbdcb
Michael <michael.ou@slu.edu>
2019-05-17 10:55:53 -0500

    read files using fs

--- app.js
+++ app.js
@@ -13,13 +13,25 @@
 //   console.log(`Server running at http://${hostname}:${port}/`);
 // });
 
-const http = require('http');
-const port=process.env.PORT || 3000
-const server = http.createServer((req, res) => {
-res.statusCode = 200;
-res.setHeader('Content-Type', 'text/html');
-res.end('<h1>Hello World</h1>');
-});
-server.listen(port,() => {
-console.log(`Server running at port `+port);
-});
+
+// WORKING AZURE
+// const http = require('http');
+// const port=process.env.PORT || 3000
+// const server = http.createServer((req, res) => {
+// res.statusCode = 200;
+// res.setHeader('Content-Type', 'text/html');
+// res.end('<h1>Hello World</h1>');
+// });
+// server.listen(port,() => {
+// console.log(`Server running at port `+port);
+// });
+
+var http = require('http');
+var fs = require('fs');
+http.createServer(function (req, res) {
+  fs.readFile('index.html', function(err, data) {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write(data);
+    res.end();
+  });
+}).listen(8080);
