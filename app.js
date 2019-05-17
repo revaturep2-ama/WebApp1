4fb735c7b7e38e8bc993cbe6aae444678eb8b7ad
Michael <michael.ou@slu.edu>
2019-05-17 09:24:47 -0500

    new app.js

--- app.js
+++ app.js
@@ -1,14 +1,25 @@
+// const http = require('http');
+
+// const hostname = '0.0.0.0';
+// const port = 3000;
+
+// const server = http.createServer((req, res) => {
+//   res.statusCode = 200;
+//   res.setHeader('Content-Type', 'text/plain');
+//   res.end('Hello World\n');
+// });
+
+// server.listen(port, hostname, () => {
+//   console.log(`Server running at http://${hostname}:${port}/`);
+// });
+
 const http = require('http');
-
-const hostname = '0.0.0.0';
-const port = 3000;
-
+const port=process.env.PORT || 3000
 const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World\n');
+res.statusCode = 200;
+res.setHeader('Content-Type', 'text/html');
+res.end('<h1>Hello World</h1>');
 });
-
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+server.listen(port,() => {
+console.log(`Server running at port `+port);
 });
