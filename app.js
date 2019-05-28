6121d681775a7539b16bb6b7dc04ec5468d49b03
alex <alex>
2019-05-27 20:50:40 -0500

    working app

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const router = express.Router();
+var shell = require('shelljs');
+var bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: false }));
+
 
 router.get('/',function(req,res){
   res.sendFile(path.join(__dirname+'/index.html'));
@@ -12,12 +16,27 @@ router.get('/about',function(req,res){
   res.sendFile(path.join(__dirname+'/about.html'));
 });
 
-router.get('/sitemap',function(req,res){
-  res.sendFile(path.join(__dirname+'/sitemap.html'));
+
+app.post('/submit-student-data', function (req, res) {
+  //shell.exec('az group create -n myRG -l southcentralus');
+  var rgName1 = req.body.rgName;
+  shell.exec('az group create -n ${rgName1} -l southcentralus');
+  console.log(rgName1);
+  res.redirect('/');
+  // let name = 'works';
+  // res.send(name + ' Submitted Successfully!');
 });
 
+app.post('/submit', function (req, res) {
+  //shell.exec('az account show');
+  console.log('account');
+  res.redirect('/');
+  // let name = 'works';
+  // res.send(name + ' Submitted Successfully!');
+});
 //add the router
 app.use('/', router);
 app.listen(process.env.port || 3000);
 
 console.log('Running at Port 3000');
+
